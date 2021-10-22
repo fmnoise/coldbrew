1f69c97728a6d31d1f4aa3355ddbf1ee52f841f4
fmnoise <fmnoise@gmail.com>
2021-10-22 23:38:06 +0300

    Add url

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Easy to use Clojure wrappers for Caffeine Caching library"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "https://github.com/fmnoise/coldbrew"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.github.ben-manes.caffeine/caffeine "3.0.4"]])

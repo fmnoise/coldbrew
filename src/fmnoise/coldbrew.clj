50cd83953d3e99d636158d0760f325003357e480
fmnoise <fmnoise@gmail.com>
2021-11-04 20:19:51 +0200

    Add possibility to cache value on condition

--- src/fmnoise/coldbrew.clj
+++ src/fmnoise/coldbrew.clj
@@ -1,38 +1,57 @@
 (ns fmnoise.coldbrew
-  (:import (com.github.benmanes.caffeine.cache CacheLoader LoadingCache Caffeine)
+  (:import (com.github.benmanes.caffeine.cache Cache CacheLoader LoadingCache Caffeine)
            (java.time Duration)))
 
 (defn cached
   "Accepts a function and creates cached version of it which uses Caffeine Loading Cache.
-  Cache expiration and refresh time (in seconds) can be provided as `:expire` and `:refresh` meta to function.
-  Example:
-  ```
+  Cache options should can be provided as meta to function. Supported options are:
+  `:expire` - expiration time (in seconds)
+  `:refresh` - refresh time (in seconds)
+  `:when` - function for checking if value should be cached
+
+  Examples:
   (cached ^{:expire 86400 :refresh 36000}
    (fn [db id] (query db id)))
-  ```"
+
+  ;; calculate todays order capacity until it reaches zero
+  (cached ^{:when zero?}
+   (fn [db worker-id] (order-capacity db worker-id)))
+  "
   [f]
-  (let [cache-builder (Caffeine/newBuilder)
+  (let [^Caffeine cache-builder (Caffeine/newBuilder)
+        condition-fn (-> f meta :when)
         _ (when-let [expire (-> f meta :expire)]
             (.expireAfterWrite cache-builder (Duration/ofSeconds expire)))
         _ (when-let [refresh (-> f meta :refresh)]
             (.refreshAfterWrite cache-builder (Duration/ofSeconds refresh)))
-        cache (.build cache-builder (reify CacheLoader (load [_ key] (apply f key))))]
-    (fn [& args] (.get ^LoadingCache cache args))))
+        cache (if condition-fn
+                (.build cache-builder)
+                (.build cache-builder (reify CacheLoader (load [_ key] (apply f key)))))]
+    (if condition-fn
+      (fn [& args]
+        (let [key (or args ::nil)]
+          (or (.getIfPresent ^Cache cache key)
+              (let [computed (apply f args)]
+                (when (condition-fn computed)
+                  (.put ^Cache cache key computed))
+                computed))))
+      (fn [& args]
+        (.get ^LoadingCache cache args)))))
 
 (defmacro defcached
   "Creates a function which uses Caffeine Loading Cache under the hood.
   Function declaration is similar to defn:
   - name (symbol) with optional meta
   - args (vector)
-  - caching key (vector) with optional meta containing cache options `:expire` and `:refresh` for cache expiration and refresh time (in seconds)
+  - caching key (vector) with optional meta containing cache options (see `cached` for more details)
   - body
+
   Example:
-  ```
   (defcached customer-lifetime-value [db date {:customer/keys [id]}]
    ^{:expire 86400 :refresh 36000}
    [db date id]
    (query-customer-ltv db date id))
-  ```"
+  "
   [name args cache-key & fn-body]
   (let [fname (gensym (str name "-cached-"))]
     `(do
--- test/fmnoise/coldbrew_test.clj
+++ test/fmnoise/coldbrew_test.clj
@@ -2,10 +2,8 @@
   (:require [clojure.test :refer [deftest is]]
             [fmnoise.coldbrew :refer [defcached cached]]))
 
-(def counter (atom nil))
-
 (deftest cached-fn-test
-  (let [_ (reset! counter 0)
+  (let [counter (atom 0)
         cf (cached (with-meta (fn [a b] (swap! counter inc) (+ a b)) {:expire 3}))
         r1 (cf 1 2)
         c1 @counter
@@ -22,24 +20,52 @@
     (is (= 5 r4) "function produces correct result")
     (is (= 3 c4) "counter is affected after calling with different arguments")))
 
+(deftest cache-when-test
+  (let [store (atom 3)
+        cf (cached (with-meta (fn [] (swap! store dec)) {:when zero?}))
+        r1 (cf)
+        r2 (cf)
+        r3 (cf)
+        r4 (cf)]
+    (is (= 2 r1) "function produces correct result")
+    (is (= 1 r2) "function produces correct result")
+    (is (= 0 r3) "function produces correct result")
+    (is (= 0 r4) "function returns cached value which satisfied condition")
+    (is (= 0 @store) "function body isn't called")))
+
 (deftest defcached-test
-  (defcached cached-func [a b]
-    ^{:expire 3}
-    [a b]
-    (swap! counter inc)
-    (+ a b))
-  (let [_ (reset! counter 0)
-        r1 (cached-func 1 2)
-        c1 @counter
-        r2 (cached-func 1 2)
-        c2 @counter
-        _ (Thread/sleep 3000)
-        r3 (cached-func 1 2)
-        c3 @counter
-        r4 (cached-func 2 3)
-        c4 @counter]
-    (is (= 3 r1 r2 r3) "function produces correct result")
-    (is (= 1 c1 c2) "counter is affected only on first run")
-    (is (= 2 c3) "counter is affected after expiration")
-    (is (= 5 r4) "function produces correct result")
-    (is (= 3 c4) "counter is affected after calling with different arguments")))
+  (let [counter (atom 0)]
+    (defcached cached-func [a b]
+      ^{:expire 3}
+      [a b]
+      (swap! counter inc)
+      (+ a b))
+    (let [r1 (cached-func 1 2)
+          c1 @counter
+          r2 (cached-func 1 2)
+          c2 @counter
+          _ (Thread/sleep 3000)
+          r3 (cached-func 1 2)
+          c3 @counter
+          r4 (cached-func 2 3)
+          c4 @counter]
+      (is (= 3 r1 r2 r3) "function produces correct result")
+      (is (= 1 c1 c2) "counter is affected only on first run")
+      (is (= 2 c3) "counter is affected after expiration")
+      (is (= 5 r4) "function produces correct result")
+      (is (= 3 c4) "counter is affected after calling with different arguments"))))
+
+(deftest defcached-when-test
+  (let [store (atom 3)]
+    (defcached cached-func []
+      ^{:when zero?} []
+      (swap! store dec))
+    (let [r1 (cached-func)
+          r2 (cached-func)
+          r3 (cached-func)
+          r4 (cached-func)]
+      (is (= 2 r1) "function produces correct result")
+      (is (= 1 r2) "function produces correct result")
+      (is (= 0 r3) "function produces correct result")
+      (is (= 0 r4) "function returns cached value which satisfied condition")
+      (is (= 0 @store) "function body isn't called"))))

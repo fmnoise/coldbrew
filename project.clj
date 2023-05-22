c3177ceaf4c9c5ad2bc1b7d3de0bcdc334d92890
fmnoise <fmnoise@gmail.com>
2023-05-22 14:22:16 +0200

    VERSION 1.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.fmnoise/coldbrew "1.1.3"
+(defproject org.clojars.fmnoise/coldbrew "1.1.4"
   :description "Easy to use Clojure wrappers for Caffeine Caching library"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
